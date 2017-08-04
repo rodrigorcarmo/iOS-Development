8eadb7051cb697d5d4c01cb9f627963c40a4631f
Rodrigo Rodrigues do Carmo <rodrigorcarmo87@gmail.com>
2017-08-04 17:48:30 -0300

    Added PartyRockApp

---
+++ PartyRockApp/PartyRockApp/PartyCell.swift
@@ -0,0 +1,37 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Rodrigo on 03/08/17.
+//  Copyright © 2017 Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        
+        let url = URL(string: partyRock.imageURL)!
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                
+            }
+        }
+    }
+
+}
